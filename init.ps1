69ada53b647cc494b91dd7625b44a564554173bc
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2022-02-27 12:52:11 -0600

    adjusting init process

--- init.ps1
+++ init.ps1
@@ -7,17 +7,22 @@ if (-not (Get-InstalledScript Install-RequiredModule)) {
 }
 
 try {
-    Install-RequiredModule -RequiredModulesFile $PSScriptRoot\requiredmodules.psd1 -Repository PSGallery -Scope AllUsers
+    Install-RequiredModule -RequiredModulesFile $PSScriptRoot\requiredmodules.psd1 -TrustRegisteredRepositories -Scope AllUsers -Quiet
 } catch {
     throw "Issue installing required modules: $($_)"
 }
 
 # setup initial Secret vault, adjust the configuration as you want or need for security purposes
 try {
+    Read-Host 'Setting local SecretStore to no authentication, select N if you do not want to apply this in the next prompt (enter to continue)'
     Set-SecretStoreConfiguration -Authentication None -Interaction None
-    Register-SecretVault -Name myCredentials -ModuleName Microsoft.PowerShell.SecretStore -DefaultVault
+    if (-not (Get-SecretVault -Name myCredentials)) {
+        Register-SecretVault -Name myCredentials -ModuleName Microsoft.PowerShell.SecretStore -DefaultVault
+    } else {
+        Write-Warning 'Secret vault [myCredentials] already exists'
+    }
 
-    Wite-Host 'Vault [myCredentials] created, use Set-Secret to add the needed credential objects'
+    Write-Host 'Vault [myCredentials] can be used to store credentials used in your local scripts. Use Set-Secret to add'
 } catch {
     Write-Warning "Issue creating scripts vault: $($_)"
 }
