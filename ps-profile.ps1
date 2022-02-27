48abdfb716f33a06f424a89a4891662ff661522d
Shawn Melton <11204251+wsmelton@users.noreply.github.com>
2022-02-27 13:14:34 -0600

    pulling more into profile

--- ps-profile.ps1
+++ ps-profile.ps1
@@ -1,3 +1,6 @@
+using namespace System.Management.Automation
+using namespace System.Management.Automation.Language
+
 $PSDefaultParameterValues = @{
     "Install-Module:Scope"      = "AllUsers"
     "Install-Module:Repository" = "PSGallery"
@@ -5,13 +8,146 @@ $PSDefaultParameterValues = @{
 
 if (Get-Module oh-my-posh -ListAvailable) {
     Import-Module oh-my-posh
-    $ohMyPoshConfig = 'C:\git\prompt\oh-my-posh\oh-my-config.json'
+    $ohMyPoshConfigOriginal = 'C:\git\prompt\oh-my-posh\oh-my-config.json'
+    $ohMyPoshConfig = "$env:userprofile\oh-my-config.json"
+    try {
+        Copy-Item $ohMyPoshConfigOriginal $ohMyPoshConfig -Force
+    } catch {
+        Write-Warning "Unable to write oh-my-posh profile to $ohMyPoshConfig -- $($_)"
+    }
     oh-my-posh --init --shell pwsh --config $ohMyPoshConfig | Invoke-Expression
 }
 
 if (Get-Module Terminal-Icons -ListAvailable) {
     Import-Module Terminal-Icons
 }
+
+#region PSReadLine
+Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
+Set-PSReadLineOption -EditMode Windows
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchBackward
+
+[scriptblock]$psReadLineHistoryScriptBlock = @{
+    $pattern = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$pattern, [ref]$null)
+    if ($pattern) {
+        $pattern = [regex]::Escape($pattern)
+    }
+
+    $history = [System.Collections.ArrayList]@(
+        $last = ''
+        $lines = ''
+        foreach ($line in [System.IO.File]::ReadLines((Get-PSReadLineOption).HistorySavePath)) {
+            if ($line.EndsWith('`')) {
+                $line = $line.Substring(0, $line.Length - 1)
+                $lines = if ($lines) {
+                    "$lines`n$line"
+                }
+                else {
+                    $line
+                }
+                continue
+            }
+
+            if ($lines) {
+                $line = "$lines`n$line"
+                $lines = ''
+            }
+
+            if (($line -cne $last) -and (!$pattern -or ($line -match $pattern))) {
+                $last = $line
+                $line
+            }
+        }
+    )
+    $history.Reverse()
+
+    $command = $history | Out-GridView -Title History -PassThru
+    if ($command) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert(($command -join "`n"))
+    }
+}
+Set-PSReadLineKeyHandler -Key F7 -BriefDescription History -LongDescription 'Show command history' -ScriptBlock $psReadLineHistoryScriptBlock
+
+[scriptblock]$psReadLineMatchingBraces = @{
+    param($key, $arg)
+
+    $closeChar = switch ($key.KeyChar) {
+        <#case#> '(' { [char]')'; break }
+        <#case#> '{' { [char]'}'; break }
+        <#case#> '[' { [char]']'; break }
+    }
+
+    $selectionStart = $null
+    $selectionLength = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
+
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+    if ($selectionStart -ne - 1) {
+        # Text is selected, wrap it in brackets
+        [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $key.KeyChar + $line.SubString($selectionStart, $selectionLength) + $closeChar)
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
+    } else {
+        # No text is selected, insert a pair
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)$closeChar")
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+    }
+}
+Set-PSReadLineKeyHandler -Key ')',']','}' -BriefDescription SmartCloseBraces -LongDescription 'Insert closing braces or skip' -ScriptBlock $psReadLineMatchingBraces
+
+[scriptblock]$psReadLineSmartBackspace = @{
+    param($key, $arg)
+
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+
+    if ($line[$cursor] -eq $key.KeyChar) {
+        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+    }
+    else {
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$($key.KeyChar)")
+    }
+}
+Set-PSReadLineKeyHandler -Key Backspace -BriefDescription SmartBackspace -LongDescription "Delete previous character or matching quotes/parens/braces" -ScriptBlock $psReadLineSmartBackspace
+
+# forgot something on command, add it to history to reuse later via up arrow
+Set-PSReadLineKeyHandler -Key Alt+w `
+    -BriefDescription SaveInHistory `
+    -LongDescription "Save current line in history but do not execute" `
+    -ScriptBlock {
+    param($key, $arg)
+
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+    [Microsoft.PowerShell.PSConsoleReadLine]::AddToHistory($line)
+    [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
+}
+
+# insert text from clipboard as here string
+Set-PSReadLineKeyHandler -Key Ctrl+V `
+    -BriefDescription PasteAsHereString `
+    -LongDescription "Paste the clipboard text as a here string" `
+    -ScriptBlock {
+    param($key, $arg)
+
+    Add-Type -Assembly PresentationCore
+    if ([System.Windows.Clipboard]::ContainsText()) {
+        # Get clipboard text - remove trailing spaces, convert \r\n to \n, and remove the final \n.
+        $text = ([System.Windows.Clipboard]::GetText() -replace "\p{Zs}*`r?`n","`n").TrimEnd()
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("@'`n$text`n'@")
+    } else {
+        [Microsoft.PowerShell.PSConsoleReadLine]::Ding()
+    }
+}
+
+#endregion PSReadLine
 
 if ($psedition -ne 'Core') {
     [System.Net.ServicePointManager]::SecurityProtocol = @("Tls12", "Tls11", "Tls", "Ssl3")
@@ -60,6 +196,7 @@ function Prompt {
 }
 #>
 
+#region functions
 function rdp {
     [cmdletbinding()]
     param (
@@ -134,6 +271,7 @@ function Find-MissingCommands {
         $notFound
     }
 }
+#endregion functions
 
 #Import-Module Az.Tools.Predictor
 #Set-PSReadLineOption -PredictionSource HistoryAndPlugin
--- readme.md
+++ readme.md
@@ -4,4 +4,8 @@ This repository holds the files I use for initializing a new machine and my Powe
 
 ## oh-my-posh
 
-The original version of this theme was pulled from Scott Hanselman's Gist here: [shanselman/ohmyposhv3-v2.json](https://gist.github.com/shanselman/1f69b28bfcc4f7716e49eb5bb34d7b2c)
+The theme found in this repository was modified from Scott Hanselman's Gist here: [shanselman/ohmyposhv3-v2.json](https://gist.github.com/shanselman/1f69b28bfcc4f7716e49eb5bb34d7b2c)
+
+## Profile
+
+A large portion of the PSReadLine customizations found in the profile were modified from Scott Hanselman's profile Gist here: [shanselman/Microsoft.PowerShell_profile.ps1](https://gist.github.com/shanselman/25f5550ad186189e0e68916c6d7f44c3)
